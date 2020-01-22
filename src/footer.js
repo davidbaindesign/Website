ae623b4a2f66c051f35ba2b38bb61dc395ffa4cb
davidbaindesign <davidbaindesign@gmail.com>
2020-01-22 14:55:39 -0500

    updated date

--- src/footer.js
+++ src/footer.js
@@ -12,7 +12,7 @@ export class Footer extends Component {
           <a href="http://codepen.io/davidbaindesign/" target="_new"><button className="sideButtons"><i className="fa fa-codepen"></i></button></a>
           <a href="https://www.freecodecamp.com/davidbaindesign" target="_new"><button className="sideButtons"><i className="fa fa-fire"></i></button></a>
           <a href="https://www.linkedin.com/in/david-bain-74465b54" target="_new"><button className="sideButtons"><i className="fa fa-linkedin"></i></button></a>
-          <span>&copy; 2018 David Bain Design – All rights reserved.</span>
+          <span>&copy; 2020 David Bain Design – All rights reserved.</span>
         </div>
       </footer>
     );
