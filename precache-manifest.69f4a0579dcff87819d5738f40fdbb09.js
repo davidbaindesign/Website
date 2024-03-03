6b3a2cf8496dabbff8465a03fadbe6173b642f8c
David Bain <davidbaindesign@gmail.com>
2024-03-03 17:39:30 -0500

    Updates

---
+++ precache-manifest.69f4a0579dcff87819d5738f40fdbb09.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fd5fa0de449c508b3c5dab523a0ad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "529d1fd38cbb4c4d80b8",
+    "url": "/static/css/main.c1ba11d4.chunk.css"
+  },
+  {
+    "revision": "aa23448004c87e802fab",
+    "url": "/static/js/2.17e9efaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.17e9efaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "529d1fd38cbb4c4d80b8",
+    "url": "/static/js/main.c625ba33.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "4634449c3e8c76f0d32d5fc6ed961aa6",
+    "url": "/static/media/baindesign.4634449c.jpg"
+  },
+  {
+    "revision": "e5f1ea61ee87f0754d76953cfdff2d57",
+    "url": "/static/media/baindesign.e5f1ea61.png"
+  },
+  {
+    "revision": "7574ee735c1f9dbd5916b3bb82bec3bf",
+    "url": "/static/media/calculator.7574ee73.jpg"
+  },
+  {
+    "revision": "5de86af42e60ad1e55516fd326ae88a6",
+    "url": "/static/media/fontawesome-webfont.5de86af4.svg"
+  },
+  {
+    "revision": "9f5388fac608dfe77f4343d38abf122f",
+    "url": "/static/media/fontawesome-webfont.9f5388fa.ttf"
+  },
+  {
+    "revision": "a1a450ea331a3fc89e867ecad963b6b2",
+    "url": "/static/media/fontawesome-webfont.a1a450ea.woff2"
+  },
+  {
+    "revision": "a247f4358b253516c32943603c4c5970",
+    "url": "/static/media/fontawesome-webfont.a247f435.eot"
+  },
+  {
+    "revision": "f84ffa8dd9706d3cf7c8e800372c0cc0",
+    "url": "/static/media/fontawesome-webfont.f84ffa8d.woff"
+  },
+  {
+    "revision": "69272070a9c64236a0e087fddf7e1b45",
+    "url": "/static/media/nintendo.69272070.jpg"
+  },
+  {
+    "revision": "5f78347b1074fcb8917ac6cec9729640",
+    "url": "/static/media/pomodoro.5f78347b.jpg"
+  },
+  {
+    "revision": "f304b354c39b97fca914054707ff46e3",
+    "url": "/static/media/weather.f304b354.jpg"
+  }
+]);
